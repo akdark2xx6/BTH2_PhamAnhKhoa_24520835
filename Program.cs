d03e5c1628de80bccf78989e113255ecd536b4f4
akdark2xx6 <akdark2xx6@gmail.com>
2025-10-26 19:51:40 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("--- IN LICH ---\n");
+        while (true)
+        {
+            //Chọn dừng hoặc tiếp tục chương trình
+            Console.WriteLine("0. Dung chuong trinh\n1.Tiep tuc chuong trinh\n");
+            int test;
+            if (!int.TryParse(Console.ReadLine(), out test) || test != 1 && test != 0)
+            {
+                Console.WriteLine("\nLua chon khong hop le, vui long chon lai\n");
+                continue;
+            }
+            if (test == 0)
+            {
+                Console.WriteLine("\n[Ket thuc chuong trinh]");
+                return;
+            }
+            ThangNam tn = new ThangNam();
+            tn.Nhap();
+            tn.InLich();
+
+        }
+    }
+}
---
+++ ThangNam.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class ThangNam
+{
+    public int Thang;
+    public int Nam;
+    static bool KiemTraHopLe(int thang, int nam)
+    {
+        //Kiểm tra ngày tháng hợp lệ
+        if (thang > 0 && thang <= 12 && nam > 0)
+            return true;
+        return false;
+    }
+    public void Nhap()
+    {
+        //Hàm nhập 
+        do
+        {
+            Console.Write("\nNhap thang: ");
+            if (!int.TryParse(Console.ReadLine(), out Thang))
+            {
+                //Kiểm tra định dạng nhập vào của tháng
+                Console.WriteLine("\nDinh dang khong hop le, vui long nhap lai\n");
+                continue;
+            }
+            Console.Write("\nNhap nam: ");
+            if (!int.TryParse(Console.ReadLine(), out Nam))
+            {
+                //Kiểm tra định dạng nhập vào của năm
+                Console.WriteLine("\nDinh dang khong hop le, vui long nhap lai\n");
+                continue;
+            }
+            if (KiemTraHopLe(Thang, Nam) == false)
+            {
+                //Kiểm tra tháng năm có hợp lệ không
+                Console.WriteLine("\nNgay thang khong hop le, vui long nhap lai\n");
+                continue;
+            }
+            break;
+        }
+        while (true);
+    }
+    static int NgayTrongThang(int thang, int nam)
+    {
+        //Hàm trả về số ngày trong tháng đó
+        int x;
+        switch (thang)
+        {
+            case 2:
+                if (nam % 400 == 0 || (nam % 100 != 0 && nam % 4 == 0))
+                {
+                    x = 29;
+                }
+                else
+                    x = 28;
+                break;
+            case 4:
+                x = 30;
+                break;
+            case 6:
+                x = 30;
+                break;
+            case 9:
+                x = 30;
+                break;
+            case 11:
+                x = 30;
+                break;
+            default:
+                x = 31;
+                break;
+        }
+        return x;
+    }
+
+    public void InLich()
+    {
+        //Hàm in lịch của tháng 
+        Console.WriteLine("\nLich thang " + Thang + "/" + Nam + "\n");
+        Console.WriteLine("Sun Mon Tue Wed Thu Fir Sat");
+        DateTime dt = new DateTime(Nam, Thang, 1);
+        int Bien = (int)dt.DayOfWeek;
+        for (int x = 0; x < Bien; x++)
+        {
+            Console.Write("    ");
+        }
+        for (int x = 1; x <= NgayTrongThang(Thang, Nam); x++)
+        {
+            if (Bien > 6)
+            {
+                Bien = 0;
+                Console.WriteLine();
+            }
+            if (x < 10)
+                Console.Write(" " + x + "  ");
+            else
+                Console.Write(" " + x + " ");
+
+            Bien++;
+        }
+        Console.WriteLine("\n");
+    }
+}
